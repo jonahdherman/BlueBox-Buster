6b30564940f660117a2e97a71d85b4871bab5dc3
Jonah <shablargablargn@New-PC>
2023-10-09 10:25:16 -0500

    wish list file created, index updated to reference it

---
+++ src/WishList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const List = ({updateWishList, removeFromWishList, lineItems, list, products}) => {
+    return(
+        <div>
+            <h2>Wish List</h2>
+            <ul>
+                {
+                    lineItems.filter(lineItem => lineItem.order_id === list.id).map( lineItem => {
+                        const product = products.find(product => product.id === lineItem.product_id) || {};
+                        return (
+                            <li key={lineItem.id}>
+                                {product.name}
+                                ({lineItem.quantity})
+                                <button onClick={ () => removeFromWishList(lineItem)}>Remove From Wish List</button>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+            {
+                lineItems.filter(lineItem => lineItem.order_id === list.id).length ? <button onClick={() => {
+                    updateWishList({...list, is_wishList:false });
+                }}>Create List Item</button>: null
+            }
+        </div>
+    );
+};
+
+export default List;
