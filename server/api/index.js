f27937b5098e5618b4c213ea40930fa63ba91591
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2023-10-09 16:49:18 -0400

    fetchUsers front and backend route, display users for admin, display updateProduct component

--- server/api/index.js
+++ server/api/index.js
@@ -6,5 +6,6 @@ app.use('/products', require('./products'));
 app.use('/', require('./auth'));
 app.use('/orders', require('./orders'));
 app.use('/lineItems', require('./lineItems'));
+app.use('/users', require('./users'));
 
 module.exports = app;
