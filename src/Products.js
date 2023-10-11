5c6e45b08f6a07b9db28597ed61066ae60fc51c8
EC2 Default User <ec2-user@ip-172-31-28-118.us-east-2.compute.internal>
2023-10-11 02:36:10 +0000

    VIP products adjustment

--- src/Products.js
+++ src/Products.js
@@ -6,12 +6,7 @@ const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, c
   
   const nonVip = products.filter( product => product.vip_only === false)
   const yesVip = products.filter( product => product.vip_only === true)
-  const both = nonVip.concat(yesVip)
-  let all = []
   
-  {
-  auth.is_vip === false ? all = nonVip : all = both
-  }
   
   return (
     <div>
@@ -21,13 +16,51 @@ const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, c
           <CreateProduct createProduct={createProduct}/>
         ) : null
       }
+      {
+        auth.is_vip ? (
+        <div>
+        <h2>Vip Only!</h2>
+          <ul>
+        {
+          yesVip.map( product => {
+            
+            
+            const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
+            
+            return (
+              <li key={ product.id }>
+                {
+                  product.image ? <img src={ product.image } /> : null
+                } 
+                 <br/>
+                <Link to={`/products/${product.id}`}>
+                  {`${ product.name }`}
+                </Link>
+                {`: $${(product.price / 100).toFixed(2)}`}
+                <br></br>
+                {`${ product.description }`}
+                {
+                  auth.id ? (
+                    cartItem ? <button onClick={ ()=> updateLineItem(cartItem)}>Add Another</button>: <button onClick={ ()=> createLineItem(product)}>Add</button>
+                  ): null 
+                }
+                {
+                  auth.is_admin ? (
+                    <Link to={`/products/${product.id}/edit`}>Edit</Link>
+                  ): null
+                }
+              </li>
+            );
+          })
+        }
+      </ul>
+      </div>
+        ) : null
+      }
+      <h2>All Products</h2>
       <ul>
         {
-         
-         
-          
-          
-          all.map( product => {
+          nonVip.map( product => {
             
             
             const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
