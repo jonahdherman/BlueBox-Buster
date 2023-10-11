6421df4bdea2b18e21c96b97b3fd87d3b252405f
Matt-Fulbrite <Matt-Fulbrite>
2023-10-11 14:28:21 -0400

    sliced descriptions and added 'Read More' links

--- src/Products.js
+++ src/Products.js
@@ -2,38 +2,93 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import CreateProduct from './CreateProduct';
 
-const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, createProduct })=> {
+const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, createProduct }) => {
+
+  const nonVip = products.filter(product => product.vip_only === false)
+  const yesVip = products.filter(product => product.vip_only === true)
+
+  
   return (
     <div>
       <h2>Products</h2>
       {
         auth.is_admin ? (
-          <CreateProduct createProduct={createProduct}/>
+          <CreateProduct createProduct={createProduct} />
         ) : null
       }
+      {
+        auth.is_vip || auth.is_admin ? (
+          <div>
+            <h2>Vip Exclusive!</h2>
+            <ul>
+              {
+                yesVip.map(product => {
+                  const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
+                  const cutOff = product.description.toString().slice(0, 250)
+
+                  return (
+                    <li key={product.id}>
+                      {
+                        product.image ? <img src={product.image} /> : null
+                      }
+                      <br />
+                        {`${product.name}`}
+                      {`: $${(product.price / 100).toFixed(2)}`}
+                      <br />
+                      {`${cutOff}...`}
+                      <Link to={`/products/${product.id}`}>
+                        {`Read More`}
+                      </Link>
+                      <br></br>
+                      {
+                        auth.id ? (
+                          cartItem ? <button onClick={() => updateLineItem(cartItem)}>Add Another</button> : <button onClick={() => createLineItem(product)}>Add</button>
+                        ) : null
+                      }
+                      {
+                        auth.is_admin ? (
+                          <Link to={`/products/${product.id}/edit`}>Edit</Link>
+                        ) : null
+                      }
+                    </li>
+                  );
+                })
+              }
+            </ul>
+          </div>
+        ) : null
+      }
+      { auth.is_vip ? <h2>Standard Products</h2> : <h2>All Products</h2>}
       <ul>
         {
-          products.map( product => {
+          nonVip.map(product => {
             const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
+            const cutOff = product.description.toString().slice(0, 250)
             return (
-              <li key={ product.id }>
-                <Link to={`/products/${product.id}`}>
-                  {`${ product.name }`}
-                </Link>
-                {`: $${(product.price / 100).toFixed(2)}`}
-                <br></br>
-                {`${ product.description }`}
-                {
-                  auth.id ? (
-                    cartItem ? <button onClick={ ()=> updateLineItem(cartItem)}>Add Another</button>: <button onClick={ ()=> createLineItem(product)}>Add</button>
-                  ): null 
-                }
-                {
-                  auth.is_admin ? (
-                    <Link to={`/products/${product.id}/edit`}>Edit</Link>
-                  ): null
-                }
-              </li>
+              <li key={product.id}>
+                      {
+                        product.image ? <img src={product.image} /> : null
+                      }
+                      <br />
+                        {`${product.name}`}
+                      {`: $${(product.price / 100).toFixed(2)}`}
+                      <br />
+                      {`${cutOff}...`}
+                      <Link to={`/products/${product.id}`}>
+                        {`Read More`}
+                      </Link>
+                      <br></br>
+                      {
+                        auth.id ? (
+                          cartItem ? <button onClick={() => updateLineItem(cartItem)}>Add Another</button> : <button onClick={() => createLineItem(product)}>Add</button>
+                        ) : null
+                      }
+                      {
+                        auth.is_admin ? (
+                          <Link to={`/products/${product.id}/edit`}>Edit</Link>
+                        ) : null
+                      }
+                    </li>
             );
           })
         }
