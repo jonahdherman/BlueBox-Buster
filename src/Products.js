25c58226be11b5573b47896e289b7c4162cc1441
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2023-10-10 22:33:23 -0400

    debugging Orders and AllOrders product.price error

--- src/Products.js
+++ src/Products.js
@@ -2,41 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import CreateProduct from './CreateProduct';
 
-const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, createProduct })=> {
-  
+const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, createProduct }) => {
+
   return (
     <div>
       <h2>Products</h2>
       {
         auth.is_admin ? (
-          <CreateProduct createProduct={createProduct}/>
+          <CreateProduct createProduct={createProduct} />
         ) : null
       }
       <ul>
         {
-          products.map( product => {
+          products.map(product => {
             const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
             return (
-              <li key={ product.id }>
+              <li key={product.id}>
                 {
-                  product.image ? <img src={ product.image } /> : null
-                } 
-                 <br/>
+                  product.image ? <img src={product.image} /> : null
+                }
+                <br />
                 <Link to={`/products/${product.id}`}>
-                  {`${ product.name }`}
+                  {`${product.name}`}
                 </Link>
                 {`: $${(product.price / 100).toFixed(2)}`}
                 <br></br>
-                {`${ product.description }`}
+                {`${product.description}`}
                 {
                   auth.id ? (
-                    cartItem ? <button onClick={ ()=> updateLineItem(cartItem)}>Add Another</button>: <button onClick={ ()=> createLineItem(product)}>Add</button>
-                  ): null 
+                    cartItem ? <button onClick={() => updateLineItem(cartItem)}>Add Another</button> : <button onClick={() => createLineItem(product)}>Add</button>
+                  ) : null
                 }
                 {
                   auth.is_admin ? (
                     <Link to={`/products/${product.id}/edit`}>Edit</Link>
-                  ): null
+                  ) : null
                 }
               </li>
             );
