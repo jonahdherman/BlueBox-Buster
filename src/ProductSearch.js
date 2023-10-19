d6297e3bcfa3027c3590223a1ab8d8ea6888b474
EC2 Default User <ec2-user@ip-172-31-28-118.us-east-2.compute.internal>
2023-10-19 00:58:13 +0000

    starting product search component

---
+++ src/ProductSearch.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import CreateProduct from './CreateProduct';
+import VipProducts from './VipProducts';
+import NonVipProducts from './NonVipProducts';
+
